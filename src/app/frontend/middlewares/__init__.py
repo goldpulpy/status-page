13cb3c3209c2274122a0ec42d1ce975595e2c282
goldpulpy <sawsani1928@gmail.com>
2026-01-17 21:32:23 +0300

    feat(config): add theme configuration support

--- src/app/frontend/middlewares/__init__.py
+++ src/app/frontend/middlewares/__init__.py
@@ -1,7 +1,17 @@
 """Middlewares for the API."""
 
+from fastapi import FastAPI
+
 from .authentication import SSRAuthMiddleware
 from .cspnonce import CSPNonceMiddleware
 from .minify import HTMLMinifyMiddleware
 
-__all__ = ["CSPNonceMiddleware", "HTMLMinifyMiddleware", "SSRAuthMiddleware"]
+
+def setup_middlewares(app: FastAPI) -> None:
+    """Set up middlewares."""
+    app.add_middleware(CSPNonceMiddleware)
+    app.add_middleware(HTMLMinifyMiddleware)
+    app.add_middleware(SSRAuthMiddleware)
+
+
+__all__ = ["setup_middlewares"]
